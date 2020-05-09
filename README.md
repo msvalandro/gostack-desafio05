98e409b4bf5a653b15a957ba57ad71c04d1e1304
Matheus Valandro <msvalandro@gmail.com>
2020-05-09 16:36:10 -0300

    feat: initial setup

---
+++ README.md
@@ -0,0 +1,28 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+<h3 align="center">
+  Desafio 05: Primeiro projeto Node.js
+</h3>
+
+<p align="center">
+  Nesse desafio, você deve criar uma aplicação para armazenar transações financeiras de entrada e saída, que deve permitir o cadastro e a listagem dessas transações. Utilizando o que você aprendeu até agora no <a target="_blank" href="https://nodejs.org/en/">Node.js</a> junto ao <a target="_blank" href="https://www.typescriptlang.org/">TypeScript</a>, aplicando conceitos de models, repositories e services!
+
+</p>
+
+### Running
+
+```sh
+#install dependencies
+yarn
+
+#start application at http://localhost:3333
+yarn dev:server
+```
+
+### Environment
+
+- Node.js v12.14.1
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((sum, { value, type }) => {
+      if (type === 'income') {
+        return sum + value;
+      }
+
+      return sum;
+    }, 0);
+    const outcome = this.transactions.reduce((sum, { value, type }) => {
+      if (type === 'outcome') {
+        return sum + value;
+      }
+
+      return sum;
+    }, 0);
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
